5f6c451d0c0bf34ec85a24168de58c47e5c97c1f
lpozo1990 <lpozo1990@gmail.com>
2022-07-21 13:30:47 -0400

    WIP Login

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@GetMapping()
+	public String greeting(Model model) {
+		model.addAttribute("data", "this is from controller");
+		return "index";
 	}
-
-}\
+}
